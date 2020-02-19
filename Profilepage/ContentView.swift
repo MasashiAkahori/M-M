2b4a51594a523a61d64e18c6d91cfc89b82891ee
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2020-02-20 01:56:25 +0900

    testing

--- Profilepage/ContentView.swift
+++ Profilepage/ContentView.swift
@@ -10,12 +10,122 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Profile()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Profile()
     }
 }
+
+
+struct Profile: View {
+    var body: some View{
+        ZStack{
+            Image("bg").resizable().edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    Button(action: {
+                        
+                    }){
+                        Image("menu").resizable().renderingMode(.original).resizable().frame(width: 20, height: 20)
+                    }
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }){
+                        Image("close").resizable().renderingMode(.original).resizable().frame(width: 20, height: 20)
+                    }
+                    
+                    
+                    
+                    
+                    
+                }
+                Spacer()
+                
+                ZStack(alignment: .top){
+                    VStack{
+                        HStack{
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Bella,").font(.title)
+                                Text("25")
+                            }
+                            Spacer()
+                            
+                            HStack(spacing: 8){
+                                Image("map").resizable().frame(width: 15, height: 20)
+                                Text("8Miles")
+                                
+                            }.padding(8)
+                                .background(Color.black.opacity(0.1))
+                                .cornerRadius(10)
+                            
+                            
+                        }.padding(.top,30)
+                        
+                        Text("real slim shady").padding(.top)
+                    }.padding()
+                        .background(BlurView())
+                        //backgroundcolor が半透明になる
+                        .clipShape(BottomShape())
+                        .cornerRadius(25)
+
+                    ZStack{
+                        Button(action: {
+                            
+                        }) {
+                            Image("play").renderingMode(.original).resizable().frame(width: 20, height: 20)
+                                .padding(20)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        }
+                        
+                        Circle().stroke(Color.yellow, lineWidth: 5).frame(width: 70, height: 70)
+                    }.offset(y: -35)
+                    
+                    
+                }
+            }
+        }
+        
+    }
+}
+    
+    
+    struct BottomShape : Shape {
+        func path(in rect: CGRect) -> Path {
+            return Path{path in
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: 0, y: rect.height))
+                path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+                path.addLine(to: CGPoint(x: rect.width, y: 0))
+                path.addArc(center: CGPoint(x: rect.width / 2, y: 0), radius: 40, startAngle: .zero, endAngle: .init(degrees: 180), clockwise: false)
+                
+                
+            }
+            
+        }
+    }
+    
+    
+    struct BlurView: UIViewRepresentable{
+        //背景を半透明にするstruct
+        
+        func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIVisualEffectView{
+            let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterialLight))
+            
+            return view
+        }
+        
+        func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<BlurView>)
+        {
+            
+        }
+}
+
+
+
