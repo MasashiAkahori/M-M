9099c647e54c0064a6b41d6f7cb80e9287c730e0
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2020-02-19 22:35:22 +0900

    Initial Commit

---
+++ Profilepage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Profilepage
+//
+//  Created by 赤堀雅司 on 19/2/20.
+//  Copyright © 2020 赤堀雅司. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProfilepageTests/ProfilepageTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProfilepageTests.swift
+//  ProfilepageTests
+//
+//  Created by 赤堀雅司 on 19/2/20.
+//  Copyright © 2020 赤堀雅司. All rights reserved.
+//
+
+import XCTest
+@testable import Profilepage
+
+class ProfilepageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
